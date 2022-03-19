9dfe0c0ebc2fc492f2132bbd43cc8130c115c504
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-03-19 14:39:53 -0600

    menu bar

--- src/app/components/shared/menu-side/menu-side.component.ts
+++ src/app/components/shared/menu-side/menu-side.component.ts
@@ -7,13 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuSideComponent implements OnInit {
 
-  menuList: string[] = [
-    'Deal',
-    'Feed',
-    'stadistics',
-    'Teams',
-    'Projects',
-    'Settings',
+  menuList: any[] = [
+    {name: 'Deal',
+    icon: 'fa fa-table-columns'
+  },
+    {name: 'Feed',
+    icon: 'fa-solid fa-square-poll-horizontal'
+  },
+    {name: 'stadistics',
+    icon: 'fa-solid fa-chart-line'
+  },
+    {name: 'Teams',
+    icon: 'fa-solid fa-people-group'
+  },
+    {name: 'Projects',
+    icon: 'fa-solid fa-list-check'
+  },
+    {name: 'Settings',
+    icon: 'fa-solid fa-gear'
+  },
   ];
 
   constructor() { }
